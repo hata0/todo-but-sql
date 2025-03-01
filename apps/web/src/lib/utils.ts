caf41f212b2848e87962e5b348d00aa256409fcc
hata0 <haataa0100@gmail.com>
2025-03-01 22:42:23 +0900

    fix: twMergeが機能しない不具合を修正

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,30 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+export const customTwMerge = extendTailwindMerge<never, "borderRadius">({
+  extend: {
+    theme: {
+      spacing: [
+        "padding-8",
+        "padding-12",
+        "padding-16",
+        "spacer-small",
+        "spacer-normal",
+      ],
+      // なんかこうすると動いた
+      borderRadius: [
+        "radius-none",
+        "radius-xs",
+        "radius-sm",
+        "radius-md",
+        "radius-lg",
+        "radius-xl",
+        "radius-full",
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
