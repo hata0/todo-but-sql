5634923949fae4cd7187dd8ca48c548a393de812
hata0 <haataa0100@gmail.com>
2025-03-02 12:20:49 +0900

    feat: IconButtonを少し作成

--- apps/web/src/components/my-ui/icon-button/index.stories.tsx
+++ apps/web/src/components/my-ui/icon-button/index.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { Heart } from "lucide-react";
 import { IconButton } from ".";
 
 type Story = StoryObj<typeof IconButton>;
@@ -8,4 +9,7 @@ export const Default: Story = {};
 export default {
   title: "my-ui/icon-button",
   component: IconButton,
+  args: {
+    children: (props) => <Heart {...props} />,
+  },
 } satisfies Meta<typeof IconButton>;
