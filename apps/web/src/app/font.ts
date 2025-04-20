f8f9eb18568dba8049a755af910e97eda63a75bc
hata0 <haataa0100@gmail.com>
2025-04-20 13:55:43 +0900

    feat: フォント関連を改善

--- apps/web/src/app/font.ts
+++ apps/web/src/app/font.ts
@@ -1,9 +1,8 @@
-import { Noto_Sans_JP } from "next/font/google";
+// const roboto = Noto_Sans_JP({
+//   weight: ["400", "500", "700"],
+//   variable: "--font-noto-sans-jp",
+//   subsets: ["latin"],
+// });
 
-const roboto = Noto_Sans_JP({
-  weight: ["400", "500", "700"],
-  variable: "--font-noto-sans-jp",
-  subsets: ["latin"],
-});
-
-export const fontVariables = `${roboto.variable}`;
+// export const fontVariables = `${roboto.variable}`;
+export const fontVariables = ``;
--- apps/web/src/tests/storybook/decorators/default.tsx
+++ apps/web/src/tests/storybook/decorators/default.tsx
@@ -1,20 +1,21 @@
 import type { Decorator } from "@storybook/react";
 import { useEffect } from "react";
-import { fontVariables } from "@/app/font";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/shadcn-ui/sonner";
 
 export const DefaultDecorator: Decorator = (Story) => {
   // フォントを追加
   useEffect(() => {
-    const fontVariableArray = fontVariables.split(" ");
+    // const fontVariableArray = fontVariables.split(" ");
 
-    document.documentElement.classList.add(...fontVariableArray, "list-none");
+    // document.documentElement.classList.add(...fontVariableArray, "list-none");
+    document.documentElement.classList.add("list-none");
     return () => {
-      document.documentElement.classList.remove(
-        ...fontVariableArray,
-        "list-none",
-      );
+      // document.documentElement.classList.remove(
+      //   ...fontVariableArray,
+      //   "list-none",
+      // );
+      document.documentElement.classList.remove("list-none");
     };
   }, []);
 
