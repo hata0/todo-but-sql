c8ad73f4cbd03efb285c21366851efb412bd7590
hata0 <haataa0100@gmail.com>
2025-05-02 10:37:22 +0900

    feat: icon関連を改善

--- apps/web/src/features/top/components/top-header/index.tsx
+++ apps/web/src/features/top/components/top-header/index.tsx
@@ -12,10 +12,10 @@ export const TopHeader = () => {
 
   return (
     <header className="z-2 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 flex h-14 items-center justify-between gap-2 border-b border-dashed px-2 backdrop-blur transition duration-500 ease-in-out">
-      <Button asChild variant="ghost" size="lg">
+      <Button asChild variant="ghost" size="icon" className="sm:w-fit sm:px-5">
         <Link href="/" className={text.large.className}>
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="animate-text-gradient bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 bg-clip-text text-transparent">
+          <Image src="/logo.png" alt="logo" width={32} height={32} priority />
+          <span className="animate-text-gradient hidden bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 bg-clip-text text-transparent sm:block">
             Todo but SQL
           </span>
         </Link>
