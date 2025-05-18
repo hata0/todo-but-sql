7ce06ab9c40f03c3ca2a0008c5a207dc834b70ba
hata0 <haataa0100@gmail.com>
2025-05-18 16:58:16 +0900

    feat: QueryProviderMockを削除

--- apps/web/src/components/providers/query-provider.tsx
+++ apps/web/src/components/providers/query-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "@/lib/tanstack-query/utils";
@@ -10,18 +10,6 @@ export const QueryProvider = ({ children }: PropsWithChildren) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
-};
-
-export const QueryProviderMock = ({
-  client,
-  children,
-}: PropsWithChildren<{ client: QueryClient }>) => {
-  return (
-    <QueryClientProvider client={client}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
