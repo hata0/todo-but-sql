a84454e643ecf9735e02a62b02eb49388510af8c
hata0 <haataa0100@gmail.com>
2025-05-18 19:19:24 +0900

    feat: tabを改善

--- apps/web/src/features/top/utils/tab.ts
+++ apps/web/src/features/top/utils/tab.ts
@@ -1,18 +1,7 @@
-import { parseAsStringLiteral, useQueryState } from "nuqs";
-import { match } from "ts-pattern";
-import { ListTaskInput } from "@/infrastructure/queries/list-task-pglite";
-
-export const TABS = ["all", "uncompleted", "completed"] as const;
-
-export type Tab = (typeof TABS)[number];
-
-export const useTabQueryState = () =>
-  useQueryState("tab", parseAsStringLiteral(TABS));
-
-export const tabToQuery = (tab: Tab | null): ListTaskInput => {
-  return match(tab)
-    .returnType<ListTaskInput>()
-    .with("uncompleted", () => ({ isCompleted: false }))
-    .with("completed", () => ({ isCompleted: true }))
-    .otherwise(() => undefined);
-};
+export const TABS = {
+  All: "all",
+  Uncompleted: "uncompleted",
+  Completed: "completed",
+} as const;
+export const TABS_ARRAY = ["all", "uncompleted", "completed"] as const;
+export type Tab = (typeof TABS_ARRAY)[number];
